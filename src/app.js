1995b9af0609789c759769c706ddffcd8dcb61b0
oprokopieva382 <oprokopieva382@gmail.com>
2022-04-25 12:21:48 -0500

    icon to real data set up

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(responce) {
-  console.log(responce.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(responce.data.main.temp);
   let cityElement = document.querySelector("#city");
@@ -35,10 +34,16 @@ function displayTemperature(responce) {
   windElement.innerHTML = Math.round(responce.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(responce.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.getAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${responce.data.weather[0].icon}@2x.png`
+  );
+  iconElement.getAttribute("alt", responce.data.weather[0].description);
 }
 
 let apiKey = "31062848022c12a9b5c54447286aa8b2";
-let city = "Paris";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
