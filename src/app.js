afcd2ccf22e6f21939c78f68b79b552c80f0cbb1
oprokopieva382 <oprokopieva382@gmail.com>
2022-04-25 11:28:57 -0500

    Added main data on top left

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(responce) {
   console.log(responce.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -10,9 +33,12 @@ function displayTemperature(responce) {
   humidityElement.innerHTML = responce.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(responce.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(responce.data.dt * 1000);
 }
 
 let apiKey = "31062848022c12a9b5c54447286aa8b2";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
